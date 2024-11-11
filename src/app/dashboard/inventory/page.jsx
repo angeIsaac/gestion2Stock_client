865c118704a0bd94170f6c2374ced86d9ad20a6e
Guidy Ange Isaac <packer02andro69@gmail.com>
2024-11-11 10:06:27 +0000

    creation du composant search

--- src/app/dashboard/inventory/page.jsx
+++ src/app/dashboard/inventory/page.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const Inventaire = () => {
+  const colums = ["Refference", "Nom", "Poids", "Quantité"]
   return (
-    <div>
-        
+    <div className='flex flex-col px-4 py-3'>
+      <div className='flex flex-col md:flex-row md:justify-between justify-center md:items-center'>
+        <div>
+          <h1 className='text-lg md:text-3xl font-bold'><span className='text-orange-400 text-4xl font-extrabold'>In</span>ventaire</h1>
+        </div>
+        <div>
+          <input type="date" name="date" id="date" className='bg-blue-100 h-8 outline-none focus:ring-1 focus:ring-blue-300 py-2 px-2 rounded-md ' />
+        </div>
+      </div>
+      <div className='w-full overflow-auto'>
+        <table className='w-full'>
+          <thead>
+            <tr>
+              {
+                colums.map(item => (<th> {item} </th>))
+              }
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   )
 }
--- src/app/ui/ListItems.jsx
+++ src/app/ui/ListItems.jsx
@@ -1,10 +1,9 @@
 "use client";
-import React from 'react'
 import Image from 'next/image';
 
 const ListItems = ({ items }) => {
     return (
-        <li className='flex items-center p-5 justify-between flex-row border-b'>
+        <li className='flex items-center p-5 justify-between flex-row border-b hover:bg-gray-200'>
             <span>date</span>
             <div className='flex justify-center items-center space-x-4'>
                 <Image src={"https://img.freepik.com/vecteurs-libre/conception-du-logo-golden-bird_1195-336.jpg?t=st=1728314020~exp=1728317620~hmac=5048c001ceaa49209d8b7bbc5300876140395b9b1b2520427a15725b6e7390ac&w=740"}
