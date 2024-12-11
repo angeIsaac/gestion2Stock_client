fc5d012b78f78528099202a692a9bb1a518fb3ad
Guidy ange isaac <packer02andro69@gmail.com>
2024-12-11 22:57:00 +0000

    modification des composant

--- src/app/ui/Table.jsx
+++ src/app/ui/Table.jsx
@@ -1,9 +1,8 @@
 import { motion } from "framer-motion";
-import QueryInput from "@/app/ui/queryInput";
 
 const Table = ({ columns, renderRow, data, handler, search }) => {
     return (
-        <motion.table 
+        <motion.table
         className="w-full mt-4 table-auto min-w-[690px]"
         initial={{opacity: 0}}
         animate={{opacity:1}}
@@ -11,16 +10,14 @@ const Table = ({ columns, renderRow, data, handler, search }) => {
             <thead>
                 <tr className="text-left text-gray-500 text-sm">
                     {columns.map((col, index) => (
-                        <motion.th 
+                        <motion.th
                         initial={{opacity:0}}
                         animate={{opacity:1}}
                         transition={{duration:0.3, delay: index * 0.1}}
                             key={index} className={`${col.className}`}>
                             <div className="inline-flex space-x-5 items-center">
                                 {col.header}
-                                {col.header === "info" ? null : <QueryInput handler={handler} search={search}/>}
                             </div>
-                            
                         </motion.th>
                     ))}
                 </tr>
